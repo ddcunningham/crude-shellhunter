e51ed410d97e9150e101a2e3ff535a32189428c9
root <root@cpanel01.newpush.com>
2015-11-22 21:40:15 -0700

    Okay, I lied.  THIS is 1.0.. dry fire phase over.

--- crude-shellhunter.sh
+++ crude-shellhunter.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+logprefixpath="/var/log/crude-shellhunter"
+mkdir ${logprefixpath}
+cat $1 | xargs -I placeholder sh -c "awk -f ~/audit/crude-shellhunter/deshellswebshellsbytheseashore.awk placeholder 2>> ${logprefixpath}/error.log; echo '======' >> ${logprefixpath}/run.log; diff -U 0 placeholder placeholder.tmp >> ${logprefixpath}/run.log ; echo '======' >> ${logprefixpath}/run.log; ; mv placeholder.tmp placeholder" 2> ${logprefixpath}/error.log
 
-find -iname "testfile*" | sort | xargs -I var sh -c "awk -f deshellswebshellsbytheseashore.awk var"
-find -iname "*.tmp" | sort | xargs -I var sh -c "less var; rm var"
