529450ae698c463fe73b1e3a387aa8b232f4ca71
root <root@cpanel01.newpush.com>
2015-11-23 13:24:37 -0700

    Fixed wrapper script to preserve proper ownership on destination file

--- crude-shellhunter.sh
+++ crude-shellhunter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 logprefixpath="/var/log/crude-shellhunter"
 mkdir ${logprefixpath}
-cat $1 | xargs -I placeholder sh -c "awk -f ~/audit/crude-shellhunter/deshellswebshellsbytheseashore.awk placeholder 2>> ${logprefixpath}/error.log; echo '======' >> ${logprefixpath}/run.log; diff -U 0 placeholder placeholder.tmp >> ${logprefixpath}/run.log ; echo '======' >> ${logprefixpath}/run.log; ; mv placeholder.tmp placeholder" 2> ${logprefixpath}/error.log
+cat $1 | xargs -I placeholder sh -c "awk -f ~/audit/crude-shellhunter/deshellswebshellsbytheseashore.awk placeholder 2>> ${logprefixpath}/error.log; echo '======' >> ${logprefixpath}/run.log; diff -U 0 placeholder placeholder.tmp >> ${logprefixpath}/run.log ; echo '======' >> ${logprefixpath}/run.log; mp --no-preserve=ownership placeholder.tmp placeholder; rm placeholder.tmp" 2> ${logprefixpath}/error.log
 
